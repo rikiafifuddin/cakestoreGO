cd9b22fd4924e7c3379443b0a4020fb4fb17be5c
Afiduddin, Riki <riki.afifuddin@ecomindo.com>
2022-06-10 02:42:36 +0700

    comment yml and env (now using local db)

--- api/config/config.go
+++ api/config/config.go
@@ -9,8 +9,11 @@ func Connect() *sql.DB {
 	dbUser := "root"
 	dbPass := "root"
 	dbName := "cakestore"
+	dbHost := "172.30.16.1"
+	dbPort := "3306"
 
-	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
+	// db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
+	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@tcp("+dbHost+":"+dbPort+")/"+dbName)
 	if err != nil {
 		panic(err.Error())
 	}
